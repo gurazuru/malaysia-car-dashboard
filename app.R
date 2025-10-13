11bf6955cf9dcc95e79571c0864fa4c01a38971f
gurazuru <nafisnaim@gmail.com>
2025-10-13 23:39:01 +0800

    Added total TIV table and chart

--- app.R
+++ app.R
@@ -33,12 +33,35 @@ ui <- page_fluid(
   tags$h5("This report provides an overview of vehicle registration trends in Malaysia, 
            using JPJ data from data.gov.my.",
           style = "color: #555; margin-top: -2px;"),
+  card(
+    height = c(160),
+    card_header("data as of 30th September 2025"),
+    DTOutput("summary_table_total")
+  ),
+  card(
+    height = c(300),
+    card_header(
+      div(
+        style = "display: flex; align-items: center; justify-content: space-between;",
+        "View as: ",
+        selectInput(
+          inputId = "agg_level_ttl",
+          label = NULL,
+          choices = c("Monthly", "Quarterly", "Annually", "5-Month Average"),
+          selected = "Monthly"
+        )
+      ),
+    ),
+        plotlyOutput("trend_plot_total"),
+  ),
+  
   
   tabsetPanel(
     id = "tabs",
     tabPanel("By Make",
              layout_columns(
                col_widths = c(12),
+               
                card(
                  card_header("Monthly Vehicle Registrations by Make"),
                  DTOutput("summary_table")
@@ -69,7 +92,6 @@ ui <- page_fluid(
                      )
                    ) 
                  ),
-                 
                  
                  plotlyOutput("trend_plot_make")
                )
@@ -299,16 +321,31 @@ server <- function(input, output, session) {
    
   }
   
-  # ---- Output Data Table Function ----
+  # ---- Output Data Table Functions ----
+  data_table_TIV <- function(df) {
+    df <- df() 
+    
+    datatable(df,
+      rownames = FALSE,
+      options = list(dom = 't', ordering = FALSE),
+      class = 'cell-border stripe') |>
+      formatPercentage("Growth (MoM)", 1) |>
+      formatPercentage("Growth (YoY)", 1) |>
+      formatPercentage("Growth (YTD)", 1) |>
+      formatStyle("Growth (MoM)", color = styleInterval(c(0), c('red', 'green'))) |>
+      formatStyle("Growth (YoY)", color = styleInterval(c(0), c('red', 'green'))) |>
+      formatStyle("Growth (YTD)", color = styleInterval(c(0), c('red', 'green')))
+  }
+  
   data_table <- function(df) {
     df <- df() 
     
     datatable(df,
-              rownames = FALSE,
-              filter = 'top',
-              selection = "multiple",
-              options = list(pageLength = 10),
-              class = 'cell-border stripe') |>
+      rownames = FALSE,
+      filter = 'top',
+      selection = "multiple",
+      options = list(pageLength = 10, searching = FALSE),
+      class = 'cell-border stripe') |>
       formatPercentage("Growth (MoM)", 1) |>
       formatPercentage("Growth (YoY)", 1) |>
       formatPercentage("Growth (YTD)", 1) |>
@@ -332,6 +369,54 @@ server <- function(input, output, session) {
   })
   
   # ---- Output Plotly Function ----
+  plot_chart_total <- function(agg_choice) {
+    monthly_trends_data_ttl <- car_data |>
+      mutate(month = date_reg) |>
+      group_by(month) |>
+      summarise(registration = sum(count), .groups = "drop")
+    
+    if (!is.null(agg_choice)) {
+      monthly_trends_data_ttl <- monthly_trends_data_ttl |>
+        mutate(year = lubridate::year(month),
+               quarter = lubridate::quarter(month),
+               month_num = lubridate::month(month))
+      
+      if (agg_choice == "Quarterly") {
+        monthly_trends_data_ttl <- monthly_trends_data_ttl |>
+          group_by(year, quarter) |>
+          summarise(registration = sum(registration), .groups = "drop") |>
+          mutate(month = as.Date(paste0(year, "-", (quarter - 1)*3 +1, "-01")))
+        
+      } else if (agg_choice == "Annually") {
+        monthly_trends_data_ttl <- monthly_trends_data_ttl |>
+          group_by(year) |>
+          summarise(registration = sum(registration), .groups = "drop") |>
+          mutate(month = as.Date(paste0(year, "-01-01")))
+        
+      } else if (agg_choice == "5-Month Average") {
+        monthly_trends_data_ttl <- monthly_trends_data_ttl |>
+          arrange(month) |>
+          mutate(registration = zoo::rollmean(registration, 5, fill = NA, align = "right")) |>
+          ungroup()
+      }
+    }
+    
+    trend_plot <- ggplot(monthly_trends_data_ttl, aes(x = month, y = registration)) +
+      geom_line(linewidth = 0.4) +
+      geom_point(size = 0.8) +
+      theme_minimal(base_size = 14) +
+      labs(x = "Month", y = "Registration", color = NULL) +
+      scale_y_continuous(labels = scales::comma)
+    
+    ggplotly(trend_plot) |>
+      layout(
+        xaxis = list(
+          rangeslider = list(visible = FALSE, thickness = 0.04)
+        )
+      ) 
+    
+  }
+  
   plot_chart <- function(df, group_col, group_col_name, top_n_value, agg_choice) {
     df <- df() # call the reactive to get the data frame.
 
@@ -370,8 +455,6 @@ server <- function(input, output, session) {
       }
     }
     
-    # agg_choice <- input[[paste0("agg_level_", group_col_name_sym)]]
-    
     if (!is.null(agg_choice)) {
       monthly_trends_data <- monthly_trends_data |>
         mutate(year = lubridate::year(month),
@@ -385,16 +468,16 @@ server <- function(input, output, session) {
           mutate(month = as.Date(paste0(year, "-", (quarter - 1)*3 +1, "-01")))
         
       } else if (agg_choice == "Annually") {
-        monthly_trends_data <- monthly_trends_data %>%
-          group_by(year, {{ group_col }}) %>%
-          summarise(registration = sum(registration), .groups = "drop") %>%
+        monthly_trends_data <- monthly_trends_data |>
+          group_by(year, {{ group_col }}) |>
+          summarise(registration = sum(registration), .groups = "drop") |>
           mutate(month = as.Date(paste0(year, "-01-01")))
         
       } else if (agg_choice == "5-Month Average") {
-        monthly_trends_data <- monthly_trends_data %>%
-          arrange(month) %>%
-          group_by({{ group_col }}) %>%
-          mutate(registration = zoo::rollmean(registration, 5, fill = NA, align = "right")) %>%
+        monthly_trends_data <- monthly_trends_data |>
+          arrange(month) |>
+          group_by({{ group_col }}) |>
+          mutate(registration = zoo::rollmean(registration, 5, fill = NA, align = "right")) |>
           ungroup()
       }
     }
@@ -415,17 +498,73 @@ server <- function(input, output, session) {
       ) 
   }
   
+  # ---- Reactive summary total ----
+  summary_total <- reactive({
+    month_current_count_ttl <- car_data |>
+      filter(date_reg == month_current) |>
+      summarise(count_current = sum(count), .groups = "drop")
+    
+    month_previous_count_ttl <- car_data |>
+      filter(date_reg == month_previous) |>
+      summarise(count_previous = sum(count), .groups = "drop")
+    
+    month_previous_year_count_ttl <- car_data |>
+      filter(date_reg == month_previous_year) |>
+      summarise(count_previous_year = sum(count), .groups = "drop")
+    
+    ytd_current_count_ttl <- car_data |>
+      filter(date_reg >= ytd_current_start & date_reg <= ytd_current_end) |>
+      summarise(count_ytd_current = sum(count), .groups = "drop")
+    
+    ytd_previous_count_ttl <- car_data |>
+      filter(date_reg >= ytd_previous_start & date_reg <= ytd_previous_end) |>
+      summarise(count_ytd_previous = sum(count), .groups = "drop")
+    
+    # Add label column first
+    label_col <- tibble(category = "Total TIV")
+    
+    bind_cols(
+      label_col,
+      month_current_count_ttl,
+      month_previous_count_ttl,
+      month_previous_year_count_ttl,
+      ytd_current_count_ttl,
+      ytd_previous_count_ttl
+    ) |>
+      mutate(
+        across(starts_with("count"), \(x) replace_na(x, 0)),
+        growth_MoM = if_else(count_previous > 0, (count_current / count_previous - 1), NA_real_),
+        growth_YoY = if_else(count_previous_year > 0, (count_current / count_previous_year - 1), NA_real_),
+        growth_YTD = if_else(count_ytd_previous > 0, (count_ytd_current / count_ytd_previous - 1), NA_real_)
+      ) |>
+      arrange(desc(count_current)) |>
+      select(category, count_current, count_previous, growth_MoM, count_previous_year, growth_YoY, count_ytd_current, count_ytd_previous, growth_YTD) |>
+      rename(
+        `Total TIV` := category,
+        !!month_current_name := count_current,
+        !!month_previous_name := count_previous,
+        `Growth (MoM)` := growth_MoM,
+        !!month_previous_year_name := count_previous_year,
+        `Growth (YoY)` := growth_YoY,
+        !!year_current_name := count_ytd_current,
+        !!year_previous_name := count_ytd_previous,
+        `Growth (YTD)` := growth_YTD
+      ) 
+  })
+  
   # ---- Reactive summary ----
   summary_data <- reactive(make_summary(car_data, "maker", "Make"))
   summary_data_model <- reactive(make_summary(car_data, "model", "Model"))
   summary_data_fuel <- reactive(make_summary(car_data, "fuel_grouped", "Fuel Type"))
   
   # ---- Output 1: DataTable ----
+  output$summary_table_total <- renderDT(data_table_TIV(summary_total))
   output$summary_table <- renderDT(data_table(summary_data))
   output$summary_table_model <- renderDT(data_table(summary_data_model))
   output$summary_table_fuel <- renderDT(data_table(summary_data_fuel))
   
   # ---- Output 2: Plotly Trend ----
+  output$trend_plot_total <- renderPlotly(plot_chart_total(input$agg_level_ttl))
   output$trend_plot_make <- renderPlotly(plot_chart(summary_data, maker, "Make", input$top_n, input$agg_level))
   output$trend_plot_model <- renderPlotly(plot_chart(summary_data_model, model, "Model", input$top_n_model, input$agg_level_model))
   output$trend_plot_fuel <- renderPlotly(plot_chart(summary_data_fuel, fuel_grouped, "Fuel Type", input$top_n_fuel, input$agg_level_fuel))
@@ -434,6 +573,7 @@ server <- function(input, output, session) {
     if (input$tabs == "By Make") {
       proxy <- DT::dataTableProxy("summary_table")
       DT::selectRows(proxy, NULL)
+      
     } else if (input$tabs == "By Model") {
       proxy <- DT::dataTableProxy("summary_table_model")
       DT::selectRows(proxy, NULL)
